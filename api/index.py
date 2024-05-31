92c8d22a8109e946b863e74c37c9b7d8368b6e6b
bitak2024 <acubitak@gmail.com>
2024-06-01 01:35:46 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,21 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import json
 import os
+from passlib.context import CryptContext
 
 app = Flask(__name__)
 
-file_path = os.path.join(os.path.dirname(__file__), 'class_pred.pickle')
-with open(file_path, 'rb') as f:
+# Load models
+with open('class_pred.pickle', 'rb') as f:
     model1 = pickle.load(f)
 
-file_path = os.path.join(os.path.dirname(__file__), 'dt_model.pickle')
-with open(file_path, 'rb') as f:
+with open('dt_model.pickle', 'rb') as f:
     model2 = pickle.load(f)
 
+
+# Define a function to predict the price
 def predict_price(region, num_of_bedrooms, num_of_bathrooms, apartment_space):
     try:
         X_new = np.array([[region, num_of_bedrooms, num_of_bathrooms, apartment_space]])
@@ -23,9 +26,7 @@ def predict_price(region, num_of_bedrooms, num_of_bathrooms, apartment_space):
     except Exception as e:
         print(f"An error occurred during prediction: {e}")
         return None
-@app.route('/')
-def home():
-    return '<h3 style="color: red;">Summary:</h3><p>This Flask app serves a machine learning model to predict apartment prices based on input parameters such as region, number of bedrooms, number of bathrooms, and apartment space. Use the /predict endpoint to get predictions.</p>'
+
 
 @app.route('/predict', methods=['GET'])
 def predict():
@@ -44,15 +45,124 @@ def predict():
         apartment_space = float(apartment_space)
         predicted_price = predict_price(region, num_of_bedrooms, num_of_bathrooms, apartment_space)
         if predicted_price is not None:
-            response = jsonify({'predicted_price': f'{int(predicted_price):,}'})
-            response.headers['Access-Control-Allow-Origin'] = '*';
-            return response
+            return jsonify({'predicted_price': f'{int(predicted_price):,}'})
         else:
             return jsonify({'error': 'Prediction failed'}), 500
     except ValueError:
         return jsonify({'error': 'Invalid input types'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+# Setup password context
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+USER_DATA_FILE = 'users.json'
+
+
+def read_users():
+    if not os.path.exists(USER_DATA_FILE):
+        return []
+    with open(USER_DATA_FILE, 'r') as f:
+        return json.load(f)
+
+
+def write_users(users):
+    with open(USER_DATA_FILE, 'w') as f:
+        json.dump(users, f, indent=4)
+
+
+def hash_password(password: str):
+    return password_context.hash(password)
+
+
+def verify_user_credentials(user_email: str, user_password: str):
+    users = read_users()
+    user = next((u for u in users if u['user_email'] == user_email), None)
+    if user and password_context.verify(user_password, user['user_password']):
+        return True
+    return False
+
+
+def register_user(user_name: str, user_password: str, user_email: str, user_phone: str):
+    users = read_users()
+    users.append({
+        'user_name': user_name,
+        'user_password': hash_password(user_password),
+        'user_email': user_email,
+        'user_phone': user_phone
+    })
+    write_users(users)
+
+
+def delete_user(user_email: str):
+    users = read_users()
+    users = [user for user in users if user['user_email'] != user_email]
+    write_users(users)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    user_name = data.get('user_name')
+    user_password = data.get('user_password')
+    user_email = data.get('user_email')
+    user_phone = data.get('user_phone')
+
+    if any(u['user_email'] == user_email for u in read_users()):
+        return jsonify({'error': 'Username already registered'}), 400
+
+    register_user(user_name, user_password, user_email, user_phone)
+    return jsonify({'message': 'Registration successful'})
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    user_email = data.get('user_email')
+    user_password = data.get('user_password')
+
+    if not verify_user_credentials(user_email, user_password):
+        return jsonify({
+            'error': 'Invalid credentials'
+        }), 401
+
+    return jsonify({'message': 'Login successful'})
+
+
+@app.route("/delete", methods=["DELETE"])
+def delete():
+    data = request.get_json()
+    user_email = data.get('user_email')
+    user_password = data.get('user_password')
+
+    if not verify_user_credentials(user_email, user_password):
+        return jsonify({
+            'error': 'Invalid credentials'
+        }), 401
+
+    delete_user(user_email)
+    return jsonify({'message': 'User deleted successfully'})
+
+
+@app.route("/password/reset", methods=["PUT"])
+def reset_password():
+    data = request.get_json()
+    user_email = data.get('user_email')
+    new_password = data.get('new_password')
+
+    # Check if user_email and new_password are provided and valid
+    if not (user_email and new_password):
+        return jsonify({'error': 'Invalid user data'}), 400
+
+    users = read_users()
+    for user in users:
+        if user['user_email'] == user_email:
+            user['user_password'] = hash_password(new_password)
+            write_users(users)
+            return jsonify({'message': 'Password reset successful'})
+
+    return jsonify({'error': 'Invalid user data'}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
